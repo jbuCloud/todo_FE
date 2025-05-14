3fc422ec67cc09c46d31c8cd23c29a0f133467bb
jooeun04 <jitchive24@gmail.com>
2025-05-14 19:01:31 +0900

    ♻️logincode 업데이트

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,4 +1,3 @@
-/*
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -11,6 +10,7 @@ import Signup from './pages/Signup';
 import Header from './component/Header';
 import Navi from './component/Navi';
 import Start from './pages/Start';
+import KakaoCallback from './pages/KakaoCallback'; // 추가
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -30,75 +30,7 @@ function App() {
             />
             <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
             <Route path="/signup" element={<Signup />} />
-            <Route
-              path="/calendar"
-              element={
-                isLoggedIn ? <Calendar /> : <Navigate to="/login" replace />
-              }
-            />
-            <Route
-              path="/my"
-              element={
-                isLoggedIn ? <My /> : <Navigate to="/login" replace />
-              }
-            />
-            <Route
-              path="/routine"
-              element={
-                isLoggedIn ? <Routine /> : <Navigate to="/login" replace />
-              }
-            />
-            <Route
-              path="/todo"
-              element={
-                isLoggedIn ? <Todo /> : <Navigate to="/login" replace />
-              }
-            />
-          </Routes>
-        </div>
-
-        {isLoggedIn && <Navi />}
-      </div>
-    </Router>
-  );
-}
-
-export default App;
-*/
-// src/App.jsx
-
-
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useState } from 'react';
-
-import Calendar from './pages/Calendar';
-import My from './pages/My';
-import Routine from './pages/Routine';
-import Todo from './pages/Todo';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Header from './component/Header';
-import Navi from './component/Navi';
-import Start from './pages/Start';
-
-function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  return (
-    <Router>
-      <div className="app-wrapper">
-        {isLoggedIn && <Header />}
-
-        <div className="page-content">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                isLoggedIn ? <Navigate to="/calendar" replace /> : <Start />
-              }
-            />
-            <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/callback" element={<KakaoCallback setIsLoggedIn={setIsLoggedIn} />} />
             <Route
               path="/calendar"
               element={
